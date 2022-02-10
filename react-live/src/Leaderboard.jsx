e17272e5ccf4ebd9e92ac209ffc404a80b845eb8
gandolh <gusatualex@yahoo.com>
2022-02-10 23:41:49 +0200

    design work

--- react-live/src/Leaderboard.jsx
+++ react-live/src/Leaderboard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 const Leaderboard = () => {
-    return ( <div className='bodyLeaderboard'>
-       
-
+    return ( <div className='pageBodyLeaderboard'>
+       <svg className="leaderBoardWave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ef6951" fill-opacity="1" d="M0,256L48,245.3C96,235,192,213,288,170.7C384,128,480,64,576,80C672,96,768,192,864,208C960,224,1056,160,1152,112C1248,64,1344,32,1392,16L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+            <div className='backDroppedBody'>asd</div>
+            <div className='frontDroppedBody'>sda</div>
 
     </div> );
 }
