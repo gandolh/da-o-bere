0ea82250de82c59144b5d0881ac40face402a8c2
gandolh <gusatualex@yahoo.com>
2022-02-13 21:42:48 +0200

    trying to make auth

--- backend/.adminjs/.entry.js
+++ backend/.adminjs/.entry.js
@@ -1,1 +1,3 @@
 AdminJS.UserComponents = {}
+import Component1 from '../my-dashboard-component'
+AdminJS.UserComponents.Component1 = Component1
--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,124 @@
+const AdminJS = require("adminjs");
+const AdminJSExpress = require("@adminjs/express");
+const AdminJSMongoose = require("@adminjs/mongoose");
+const mongoose = require("mongoose");
+const express = require("express");
+const bcrypt = require('bcrypt')
+require("dotenv").config();
+// We have to tell AdminJS that we will manage mongoose resources with it
+AdminJS.registerAdapter(AdminJSMongoose);
 
-const AdminJS = require('adminjs')
-const AdminJSExpress = require('@adminjs/express')
-const AdminJSMongoose = require('@adminjs/mongoose')
-const mongoose = require('mongoose');
-const express = require('express')
-require('dotenv').config();
 
-AdminJS.registerAdapter(AdminJSMongoose)
+// express server definition
+const app = express();
+const port = process.env.PORT;
 
-const app = express()
-const port = process.env.PORT
 mongoose.connect(process.env.mongodbUrl);
-const User = mongoose.model('User', { AvatarUrl: String,TagName: String, BeerCount: Number})
-const adminJs = new AdminJS({
-  databases: [],
-  rootPath: '/admin',
-  resources: [User],
-})
+
+// Resources definitions
+const User = mongoose.model("User", { AvatarUrl: String, TagName: String, BeerCount: Number });
+const AdminUser = mongoose.model("AdminUser", {
+	userName: { type: String, required: true },
+	encryptedPassword: { type: String, required: true },
+	role: { type: String, enum: ["admin", "restricted"], required: true },
+});
+
+// Pass all configuration settings to AdminJS
+const UserControl = {
+	name: "User Control",
+	icon: "User",
+};
+const adminJsOptions = new AdminJS({
+	databases: [],
+	rootPath: "/admin",
+	branding: {
+		companyName: "Caciau Enterprise",
+	},
+	dashboard: {
+		handler: async () => {
+			console.log("1");
+		},
+		component: AdminJS.bundle("./my-dashboard-component"),
+	},
+	resources: [
+		{ resource: User, options: { parent: UserControl } },
+		{
+			resource: AdminUser,
+			options: {
+				properties: {
+					encryptedPassword: {
+						isVisible: false,
+					},
+					password: {
+						type: "string",
+						isVisible: {
+							list: false,
+							edit: true,
+							filter: false,
+							show: false,
+						},
+					},
+				},
+				actions: {
+					new: {
+						before: async (request) => {
+							if (request.payload.password) {
+								request.payload = {
+									...request.payload,
+									encryptedPassword: await bcrypt.hash(request.payload.password, 10),
+									password: undefined,
+								};
+							}
+							return request;
+						},
+					},
+				},
+				parent: UserControl,
+			},
+		},
+	],
+    actions: {
+        new: {
+            before: async (request) => {
+              if(request.payload.password) {
+                request.payload = {
+                  ...request.payload,
+                  encryptedPassword: await bcrypt.hash(request.payload.password, 10),
+                  password: undefined,
+                }
+              }
+              return request
+            },
+          },
+        edit: { isAccessible: false },
+        delete: { isAccessible: false },
+        new: { isAccessible: false },
+    }
+});
+
+const defaultRouter = AdminJSExpress.buildRouter(adminJsOptions);
+
+// Build and use a router which will handle all AdminJS routes
+const authRouter = AdminJSExpress.buildAuthenticatedRouter(adminJsOptions, {
+    authenticate: async (userName, password) => {
+      const user = await User.findOne({ userName })
+      if (user) {
+        const matched = await bcrypt.compare(password, user.encryptedPassword)
+        if (matched) {
+          return user
+        }
+      }
+      return false
+    },
+    cookiePassword: process.env.cookiePassword,
+  })
 
 
-const router = AdminJSExpress.buildRouter(adminJs)
+app.get("/", (req, res) => {
+	res.send("E viu!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.use(adminJs.options.rootPath, router)
+app.use(adminJsOptions.options.rootPath, defaultRouter);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
